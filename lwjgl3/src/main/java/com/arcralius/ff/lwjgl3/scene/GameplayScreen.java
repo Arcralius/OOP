9d2b2dca4118590c5bf845b0dbdcccde358d4300
Loh Wen Xuan <2401561@sit.singaporetech.edu.sg>
2025-02-14 00:14:06 +0800

    Update GameplayScreen.java

--- lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/scene/GameplayScreen.java
+++ lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/scene/GameplayScreen.java
@@ -1,66 +1,56 @@
 package com.arcralius.ff.lwjgl3.scene;
 
+import com.badlogic.gdx.Gdx;
 import com.arcralius.ff.lwjgl3.BaseScreen;
 import com.arcralius.ff.lwjgl3.movement.MovementController;
-import com.arcralius.ff.lwjgl3.entity.*;
-import com.arcralius.ff.lwjgl3.collision.CollisionController;
+import com.arcralius.ff.lwjgl3.scene.SceneController;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-
-import java.util.ArrayList;
 
 public class GameplayScreen extends BaseScreen {
     private final TiledMap map;
     private final OrthogonalTiledMapRenderer mapRenderer;
-    private final PlayableEntity player;
-    private final EntityController entityController;
+    private final Sprite bucketSprite;
+    private final Texture bucketTexture;
+    private final Texture backgroundTexture;
+    private final Sprite backgroundSprite;
     private final MovementController movementController;
-    private final TiledMapTileLayer collisionLayer;
-    private final CollisionController collisionController;
 
-    public GameplayScreen(SceneController sceneController) {
-        super(); //Call BaseScreen constructor
-
-        //Initialize entity controller with an empty list
-        this.entityController = new EntityController(new ArrayList<>());
-
-        //Load the tile map before initializing entities
+    public GameplayScreen(SceneController sceneController, MovementController movementController) {
+        super();
+        this.movementController = movementController;
         this.map = new TmxMapLoader().load("background.tmx");
         this.mapRenderer = new OrthogonalTiledMapRenderer(map, batch);
-
-        //Retrieve the collision layer from the map
-        this.collisionLayer = (TiledMapTileLayer) map.getLayers().get("collisions");
-
-        //Initialize collision controller
-        this.collisionController = new CollisionController(collisionLayer);
-
-        //Initialize the player entity
-        this.player = new PlayableEntity("bucket.png", 100, 100, "player", 2.0f);
-        entityController.addEntity(player); //Add player to entity list
-
-        //Initialize movement controller using `collisionController`
-        this.movementController = new MovementController(player, camera, collisionController);
-
-        //Center the camera on the player
-        camera.position.set(player.getX(), player.getY(), 0);
+        this.bucketTexture = new Texture("bucket.png");
+        this.bucketSprite = new Sprite(bucketTexture);
+        this.bucketSprite.setSize(16, 16);
+        this.backgroundTexture = new Texture("plains.png");
+        this.backgroundSprite = new Sprite(backgroundTexture);
+        backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(bucketSprite.getX(), bucketSprite.getY(), 0);
     }
 
     @Override
     protected void update(float delta) {
-        movementController.handleMovement(delta); //Player movement handled here
+        movementController.handleMovement(delta);
         camera.update();
     }
 
     @Override
     protected void draw() {
-        mapRenderer.setView(camera);
-        mapRenderer.render(); //Render the tile map
-
-        //Start batch for rendering entities
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        entityController.draw(batch);
+        backgroundSprite.draw(batch);
+        batch.end();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+        batch.begin();
+        bucketSprite.draw(batch);
         batch.end();
     }
 
@@ -68,6 +58,7 @@ public class GameplayScreen extends BaseScreen {
     public void dispose() {
         super.dispose();
         map.dispose();
-        mapRenderer.dispose();
+        bucketTexture.dispose();
+        backgroundTexture.dispose();
     }
 }
