3ab09f5848f55a79f5e8c694e13252a6702df3a3
Loh Wen Xuan <2401561@sit.singaporetech.edu.sg>
2025-02-14 00:19:16 +0800

    Update GameMaster.java

--- lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/GameMaster.java
+++ lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/GameMaster.java
@@ -1,72 +1,28 @@
 package com.arcralius.ff.lwjgl3;
-//...........................................
-import com.arcralius.ff.lwjgl3.entity.*;
-import com.arcralius.ff.lwjgl3.entity.BaseEntity;
-import com.arcralius.ff.lwjgl3.entity.EntityController;
-import com.arcralius.ff.lwjgl3.input_output.*;
-import com.arcralius.ff.lwjgl3.scene.*;
 
-//...................................................
+import com.badlogic.gdx.Game;
+import com.arcralius.ff.lwjgl3.scene.SceneController;
+import com.arcralius.ff.lwjgl3.scene.MainMenuScreen;
 
-import com.badlogic.gdx.ApplicationAdapter;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random; //random numbers
-
-
-public class GameMaster extends ApplicationAdapter{
-
+public class GameMaster extends Game {
     private SceneController sceneController;
-
-
-    public GameMaster() {
-        this.sceneController = new SceneController();
-    }
-
 
     @Override
     public void create() {
-
-        sceneController = new SceneController();
-        sceneController.changeScreen(new GameplayScreen()); // Start with GameplayScreen
-
-
-
-
+        // Pass (Game instance) to SceneController
+        sceneController = new SceneController(this);
+        sceneController.changeScreen(new MainMenuScreen(sceneController));
     }
+
     @Override
     public void render() {
-        ScreenUtils.clear(0, 0, 0.2f, 1);
-
-        sceneController.render();
-
-
-
+        super.render(); //Use Game's render method
     }
 
     @Override
     public void dispose() {
-        System.out.println("Disposing resources...");
-
-        // Dispose batch
-
-
-
-
-        // Loop through and dispose all entities
-
-
-        // Dispose EntityManager
+        if (sceneController != null) {
+            sceneController.dispose();
+        }
     }
-
-
 }
