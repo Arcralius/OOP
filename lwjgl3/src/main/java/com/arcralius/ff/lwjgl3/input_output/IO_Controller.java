feb9968f8919bdae7d8a7ac5696497fafaaf08bf
Weilon <101789649+ww00010@users.noreply.github.com>
2025-02-19 15:09:44 +0800

    Update IO_Controller

--- lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/IO_Controller.java
+++ lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/IO_Controller.java
@@ -2,22 +2,29 @@ package com.arcralius.ff.lwjgl3.input_output;
 
 public class IO_Controller {
     private KeyboardManager keyboardManager;
+    private MouseManager mouseManager;
     private DisplayManager displayManager;
     private AudioManager audioManager;
 
     public IO_Controller() {
         this.keyboardManager = new KeyboardManager();
+        this.mouseManager = new MouseManager();
         this.displayManager = new DisplayManager();
         this.audioManager = new AudioManager();
     }
 
     public void update() {
         keyboardManager.pollInput();
+        mouseManager.pollInput();
         displayManager.renderFrame();
     }
 
     public KeyboardManager getKeyboardManager() {
         return keyboardManager;
+    }
+
+    public MouseManager getMouseManager() {  // Added getter for MouseManager
+        return mouseManager;
     }
 
     public DisplayManager getDisplayManager() {
