0f794d46db6ccab7a5894742996cc4d1ed5a19e8
Weilon <101789649+ww00010@users.noreply.github.com>
2025-02-19 14:52:16 +0800

    Update KeyboardManager and IO_Controller

--- lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/IO_Controller.java
+++ lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/IO_Controller.java
@@ -1,22 +1,23 @@
 package com.arcralius.ff.lwjgl3.input_output;
 
 public class IO_Controller {
-    private InputManager inputManager;
+    private KeyboardManager keyboardManager;
     private DisplayManager displayManager;
     private AudioManager audioManager;
 
     public IO_Controller() {
-        this.inputManager = new InputManager();
+        this.keyboardManager = new KeyboardManager();
         this.displayManager = new DisplayManager();
         this.audioManager = new AudioManager();
     }
 
     public void update() {
-
+        keyboardManager.pollInput();
+        displayManager.renderFrame();
     }
 
-    public InputManager getInputManager() {
-        return inputManager;
+    public KeyboardManager getKeyboardManager() {
+        return keyboardManager;
     }
 
     public DisplayManager getDisplayManager() {
--- lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/KeyboardManager.java
+++ lwjgl3/src/main/java/com/arcralius/ff/lwjgl3/input_output/KeyboardManager.java
@@ -1,23 +1,35 @@
 package com.arcralius.ff.lwjgl3.input_output;
 
-public class KeyboardManager extends InputManager{
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyboardManager extends InputManager {
+    private Map<Integer, Boolean> keyStates;
 
     public KeyboardManager() {
         super();
+        keyStates = new HashMap<>();
     }
 
     @Override
     public void pollInput() {
-        // Simulated keyboard polling (replace with actual implementation)
-        System.out.println("Polling keyboard...");
+        // Update key states for movement keys
+        keyStates.put(Input.Keys.W, Gdx.input.isKeyPressed(Input.Keys.W));
+        keyStates.put(Input.Keys.A, Gdx.input.isKeyPressed(Input.Keys.A));
+        keyStates.put(Input.Keys.S, Gdx.input.isKeyPressed(Input.Keys.S));
+        keyStates.put(Input.Keys.D, Gdx.input.isKeyPressed(Input.Keys.D));
+
+        keyStates.put(Input.Keys.SPACE, Gdx.input.isKeyPressed(Input.Keys.SPACE));
+        keyStates.put(Input.Keys.ESCAPE, Gdx.input.isKeyPressed(Input.Keys.ESCAPE));
     }
 
-    public boolean isKeyPressed(String key) {
-        return true;
-
+    public boolean isKeyPressed(int key) {
+        return keyStates.getOrDefault(key, false);
     }
 
-    public boolean isKeyReleased(String key) {
-       return true;
+    public boolean isKeyReleased(int key) {
+        return !isKeyPressed(key);
     }
 }
