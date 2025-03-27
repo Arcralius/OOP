532a22cb32c998e297e7026b2f69bc45de9f1ac1
Arcralius <owq111@gmail.com>
2025-03-27 20:03:44 +0800

    Fixed asset bug

--- assets/kitchen.tsx
+++ assets/kitchen.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="kitchen" tilewidth="16" tileheight="16" tilecount="333" columns="9">
- <image source="C:/Users/owq11/Downloads/DO NOT DEL/Kitchen and more tileset [16x16]/tileset.png" trans="ff00ff" width="144" height="592"/>
+ <image source="tileset.png" trans="ff00ff" width="144" height="592"/>
 </tileset>
